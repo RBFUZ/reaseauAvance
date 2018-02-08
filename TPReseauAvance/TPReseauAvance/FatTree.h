f162206d51adb2748d75e343cccbb2b76d08f38e
Remy Bouteloup <remy.bouteloup@etu.univ-tours.fr>
2018-02-08 16:12:40 +0100

    Create FatTree.h

---
+++ TPReseauAvance/TPReseauAvance/FatTree.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <fstream>
+
+class FatTree
+{
+public:
+	FatTree(int k);
+	void printEntete();
+	void printNode();
+	void closeFile();
+
+private:
+	int k, nbServeurAll, nbServeur, nbEdgeAll, nbEdge, nbCore;
+	std::ofstream fichier;
+};
+
