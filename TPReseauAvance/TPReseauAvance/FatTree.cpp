6d52755677074d039684fe4a36cb96988802202b
Bouteloup <remy.bouteloup@yahoo.fr>
2018-02-08 14:31:27 +0100

    update main

--- TPReseauAvance/TPReseauAvance/FatTree.cpp
+++ TPReseauAvance/TPReseauAvance/FatTree.cpp
@@ -1,5 +1,7 @@
 #include "FatTree.h"
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -11,5 +13,13 @@ FatTree::FatTree(void)
 
 int main(int argc, char *argv[])
 {
+	// Création et ouverture en écritre du fichier topogen.cpp
+	ofstream fichier("topogen.cpp", ios::out | ios::trunc);
+
+	// 
+	fichier << "#fat topology file.";
+
+	fichier.close();
+
 	return 0;
 }
---
+++ TPReseauAvance/TPReseauAvance/topogen.cpp
@@ -0,0 +1,1 @@
+#fat topology file.
